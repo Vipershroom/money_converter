2cff9271f3ad9547da906b1af73749ff1af2b6b6
Vipershroom <undertalefan812@gmail.com>
2024-01-25 10:13:08 -0500

    Create remainder function

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Scanner;
 import java.util.HashMap;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -6,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Money Converter");
         String[] money_list = handle_input();
+        calculate(money_list);
     }
 
     public static String[] handle_input() {
@@ -22,7 +24,36 @@ public class Main {
     }
 
     public static void calculate(String[] money_list) {
-        HashMap<String, Integer> money_hash = new HashMap<String, Integer>();
+        // Create Coin hashmap
+        HashMap<String, Integer> coins = new HashMap<String, Integer>();
+        coins.put("Quarters", 0);
+        coins.put("Dimes", 0);
+        coins.put("Nickels", 0);
+        coins.put("Pennies", 0);
+
+        int dollar = Integer.parseInt(money_list[0]);
+        double cents = Double.parseDouble(money_list[1]);
+
+        coins.put("Quarters",dollar);
+
+        double quarters = Math.floor(cents / 25);
+        double quarters_remainder = remainder("Quarter", cents);
+        System.out.println(quarters);
+        System.out.println(quarters_remainder);
+
+        System.out.println();
+
+    }
+
+    public static double remainder(String type, double num) {
+        if (type.equals("Quarter")) {
+            return num % 25;
+        } else if (type.equals("Dime")) {
+            return num % 10;
+        } else if (type.equals("Nickel")) {
+            return num % 5;
+        }
+        return num;
     }
 
 }
